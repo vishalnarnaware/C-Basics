701e563882345716d56a6d9179d1d76b61dbd0d4
vishalnarnaware <narnaware_vishal.ghrceai@raisoni.net>
2020-06-18 13:45:49 +0530

    Simple if

---
+++ Day3/if.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+  int a = 100;
+  int b = 200;
+  int greater;
+  if(a>b)   {
+      printf("a is greater!: %d", a);
+  }
+  if(b>a)   {
+      printf("b is greater!: %d", b);
+  }
+  return 0;
+}
