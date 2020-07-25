1c5494af92602170e9b5a62375bbff7a2427a988
vishalnarnaware <narnaware_vishal.ghrceai@raisoni.net>
2020-07-25 15:52:27 +0530

    Palindrome check done

--- Practical/Practical2/stacks.c
+++ Practical/Practical2/stacks.c
@@ -1,30 +1,55 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #define MAX 5   // maximum 5 elements can be inserted
 
-void push(int stack[MAX], int *top) {
+void push(int stack[MAX], int *top, int ele) {
     // function to insert element into Stack
     if (*top >= MAX) {
         printf("\n\tOverflow! Resetting Stack...");
         *top = 0;       // reset top to 0
     }
     else    {
-        printf("\nEnter element: ");
-        scanf("%d", &stack[*top]);
+        stack[*top] = ele;
         *top += 1;      // increment top by 1
     }
 }
-void pop(int stack[MAX], int *top)  {
+int pop(int stack[MAX], int *top)  {
     // function to delete element from Stack
     if(*top <= 0)   {
         printf("\nUnderflow! Enter elements in Stack...");
+        return -1;
     }
     else    {
         *top -= 1;      // decrement top
-        printf("\nPopping off element: %d", stack[*top]);   // display deleted
+        return stack[*top];
     }
 }
 void check()    {
     // function to check if given number is palindrome or not
+    char s[MAX];
+    int stack[MAX];
+    int i, flag=0, top=0, temp;
+    printf("\nEnter any number: ");
+    scanf("%s", &s);
+    for (i=0; i<strlen(s); i++)    {
+        temp = s[i];
+        push(stack, &top, temp);
+    }
+
+    for (i=0; i<strlen(s)/2; i++)  {
+        if (stack[i] != pop(stack, &top))   {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 1)  {
+        printf("\nNumber isn't Palindrome");
+    }
+    else    {
+        printf("\nNumber is Palindrome");
+    }
+
 }
 void display(int stack[MAX], int *top)  {
     // function to show elements of Stack
@@ -36,7 +61,7 @@ void display(int stack[MAX], int *top)  {
 }
 
 int main(int argc, char const *argv[]) {
-    int i, top=0, ch;
+    int i, top=0, ch, temp;
     int stack[MAX];
 
     while(1)    {       // Repeat again and again
@@ -49,9 +74,12 @@ int main(int argc, char const *argv[]) {
         printf("\nEnter your choice: ");            // Ask user for operation
         scanf("%d", &ch);                           // Get in ch
         switch(ch)  {                               // Driving block
-            case 1: push(stack, &top);
+            case 1: printf("\nEnter element: ");
+                    scanf("%d", &temp);
+                    push(stack, &top, temp);
                     break;
-            case 2: pop(stack, &top);
+            case 2: temp = pop(stack, &top);
+                    printf("\nPopped off element: %d", temp);   // display deleted
                     break;
             case 3: check();
                     break;
