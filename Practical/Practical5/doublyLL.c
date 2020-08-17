a65a73d918b9bb7e028fb8c8b283c5693dc2eead
vishalnarnaware <narnaware_vishal.ghrceai@raisoni.net>
2020-08-17 11:16:12 +0530

    Implement Deletion Operation: Function

--- Practical/Practical5/doublyLL.c
+++ Practical/Practical5/doublyLL.c
@@ -22,6 +22,26 @@ void insert(struct Node** head_ref, int new_data)
     (*head_ref) = new_node;
 }
 
+void deleteNode(struct Node **head_ref, int key)
+{
+	struct Node* temp = *head_ref, *prev;
+	if (temp != NULL && temp->data == key)
+	{
+		*head_ref = temp->next;
+		free(temp);
+		return;
+	}
+	while (temp != NULL && temp->data != key)
+	{
+		prev = temp;
+		temp = temp->next;
+	}
+	if (temp == NULL) return;
+	prev->next = temp->next;
+
+	free(temp);
+}
+
 void printList(struct Node* node)
 {
     struct Node* last;
@@ -52,6 +72,6 @@ int main(int argc, char const *argv[]) {
 
     printf("\n Created Doubly Linked List: ");
     printList(head);
-    
+
     return 0;
 }
