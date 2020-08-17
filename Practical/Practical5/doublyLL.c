5b724d57eac3e448f89873958be387b8503b6dc5
vishalnarnaware <narnaware_vishal.ghrceai@raisoni.net>
2020-08-17 11:11:12 +0530

    Add Doubly Linked List Nodes

--- Practical/Practical5/doublyLL.c
+++ Practical/Practical5/doublyLL.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
 int main(int argc, char const *argv[]) {
-    
+
     return 0;
 }
