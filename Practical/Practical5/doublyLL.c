0b58bed40c5bf3de4d757d72ddc383c958e569a3
vishalnarnaware <narnaware_vishal.ghrceai@raisoni.net>
2020-08-17 11:13:52 +0530

    Insert Function to add Nodes

--- Practical/Practical5/doublyLL.c
+++ Practical/Practical5/doublyLL.c
@@ -7,6 +7,21 @@ struct Node {
     struct Node* prev;
 };
 
+void insert(struct Node** head_ref, int new_data)
+{
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+
+    new_node -> data = new_data;
+
+    new_node -> next = (*head_ref);
+    new_node -> prev = NULL;
+
+    if((*head_ref) != NULL)
+        (*head_ref) -> prev = new_node;
+
+    (*head_ref) = new_node;
+}
+
 int main(int argc, char const *argv[]) {
     struct Node* head = NULL;
     int temp, n, i;
