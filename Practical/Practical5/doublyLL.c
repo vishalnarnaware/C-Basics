7a072f30ff97cb59575782f425643e0838035974
vishalnarnaware <narnaware_vishal.ghrceai@raisoni.net>
2020-08-17 11:12:41 +0530

    Insert nodes in DLL

--- Practical/Practical5/doublyLL.c
+++ Practical/Practical5/doublyLL.c
@@ -8,6 +8,21 @@ struct Node {
 };
 
 int main(int argc, char const *argv[]) {
+    struct Node* head = NULL;
+    int temp, n, i;
 
+    printf("\n Program Author: Vishal Narnaware");
+    printf("\n Branch: Artificial Intelligence Engineering");
+    printf("\n Section: A \t Semester: III");
+    printf("\n Roll Number: 63");
+
+    printf("\n Enter number of elements to be inserted: ");
+    scanf("%d", &n);
+
+    for(i=0; i<n; i++)  {
+        printf(" Enter element %d: ", i+1);
+        scanf("%d", &temp);
+        insert(&head, temp);
+    }
     return 0;
 }
